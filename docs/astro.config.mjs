ef8fff5796f319147176f821d31add0d784319b8
sulimninkaev <sulimninkaev0@gmail.com>
2025-08-14 10:13:48 +0200

    v1

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,63 +1,15 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import starlightObsidianTheme from 'starlight-theme-obsidian';
-import starlightLinksValidator from 'starlight-links-validator';
+import starlight from '@astrojs/starlight'
+import { defineConfig } from 'astro/config'
+import starlightThemeObsidian from 'starlight-theme-obsidian'
 
 export default defineConfig({
-    site: "https://fevol.github.io",
-    base: "/starlight-theme-obsidian",
     integrations: [
         starlight({
-            title: 'Starlight Obsidian Theme',
-            credits: true,
-            social: [
-				{ icon: 'github', label: 'GitHub', href: 'https://github.com/fevol/starlight-theme-obsidian'},
-				{ icon: 'discord', label: 'Discord', href: 'https://discord.com/users/264169866511122432' },
-            ],
-            editLink: {
-                baseUrl: 'https://github.com/fevol/starlight-theme-obsidian/edit/main/docs/',
-            },
-            customCss: [
-                './src/styles/global.css'
-            ],
-            plugins: [
-                starlightLinksValidator({
-                    errorOnInvalidHashes: false
-                }),
-                starlightObsidianTheme(),
-            ],
-            favicon: './favicon.svg',
-            sidebar: [
-                {
-                    label: 'Start Here',
-                    items: [
-                        { label: 'Getting Started', link: '/getting-started/' },
-                        { label: 'Contributing', link: '/contributing/' },
-                        { label: 'Attribution', link: '/attribution/' },
-                        { label: 'Changelog', link: '/changelog/' },
-                    ],
-                },
-				{
-					label: 'Resources',
-					autogenerate: { directory: 'resources' },
-				},
-                {
-                    label: 'Configuration',
-                    autogenerate: { directory: 'configuration' },
-                },
-                {
-                    label: 'Components',
-                    autogenerate: { directory: 'components' },
-                },
-                {
-                    label: 'Examples',
-                    autogenerate: { directory: 'examples' },
-                },
-            ],
-            components: {
-                Head: './src/overrides/Head.astro',
-            },
+            plugins: [starlightThemeObsidian({
+                   backlinks: false,
+                   graph: false
+            })],
+            title: 'My Docs',
         }),
     ],
-    devToolbar: { enabled: false },
-});
+})
